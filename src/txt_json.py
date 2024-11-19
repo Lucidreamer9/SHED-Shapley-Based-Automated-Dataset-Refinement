b0607ab5a9840595fea7f032104c77ac429c547f
Lucidreamer9 <143241355+Lucidreamer9@users.noreply.github.com>
2024-11-18 23:13:07 -0500

    Update txt_json.py

--- src/txt_json.py
+++ src/txt_json.py
@@ -1,12 +1,12 @@
 import json
 
-
+num_clusters = int(sys.argv[1])
 obj_temp=[]
-with open("/workspace/cluster_center.txt", "r+", encoding="utf-8") as ori:
+with open("/workspace/cluster_center_"+str(num_clusters)+".txt", "r+", encoding="utf-8") as ori:
     item: str
     for item in ori:
         obj_temp.append(eval(item))
 
-json_temp=open("/workspace/cluster_center.json", "w", encoding="utf-8")
+json_temp=open("/workspace/cluster_center_"+str(num_clusters)+".json", "w", encoding="utf-8")
 h=json.dumps(obj_temp,indent=1)
 json_temp.write(h)
